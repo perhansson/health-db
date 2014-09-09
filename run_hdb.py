557f6a148badad7c7bea4ad57f208cfdd97cf1c6
Per Hansson Adrian <per.hansson80@gmail.com>
2014-09-09 00:07:56 -0700

    added first db usage for foods

--- run_hdb.py
+++ run_hdb.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 import sys
 import getopt
+import MySQLdb
 
 def usage():
     print "Usage: ",sys.argv[0], " [hd]"
-
 
 def parseArgs(argv):
     try:
@@ -21,23 +21,84 @@ def parseArgs(argv):
             _debug = 1                  
 
 class Food:
-    strFormat = "<name>,<amount>,<amount unit>,<energy> [kcal],carbs [g],protein [g],fat[g],comment"
-    def __init__(self,str):
-        self.identifier = ""
-        self.name = ""
-        self.amount = 0.0
-        self.amount_unit = ""
-        self.E = 0.0
-        self.carbs = 0.0
-        self.protein = 0.0
-        self.fat = 0.0
-        self.comment = ""
-        self.parse(str)
-    def parse(self,str):
-        print "should parse the input data here"
+    strFormat = "name,energy [kcal],carbs [g],protein [g],fat[g],comment"
+    host="localhost"
+    port=3306
+    user="root"
+    pw="pellestjej"
+    dbname="hdb"
+    tablename="foodtest"
+    def __init__(self):
+        """creating food"""
+    def set(self,str):
+        s = str.split(",")
+        self.name = s[0]
+        self.E = float(s[1])
+        self.carbs = float(s[2])
+        self.protein = float(s[3])
+        self.fat = float(s[4])
+        self.comment = s[5]
     def ok(self):
         """check if this is ok"""
         return True
+    @staticmethod
+    def printFoodList():
+        foods = Food.getFoodList()
+        s = ""
+        i=0
+        for food in foods:
+            s += "[" + str(i) + "] "
+            for col in food:
+                s += str(col) + " "
+            s += "\n" 
+            i=i+1
+        print "List of foods available (total %d): \n%s" % (len(foods),s)
+    @staticmethod
+    def getFoodList():
+        sqlq = """SELECT * FROM foodtest"""
+        db = MySQLdb.connect(host=Food.host, port=Food.port, user=Food.user, passwd=Food.pw, db=Food.dbname)
+        cursor = db.cursor()
+        data = None
+        try:
+            cursor.execute(sqlq)
+            data = cursor.fetchall()
+            #print "Result is %d long:" % len(data)
+            #for row in data:
+            #    print row
+                #for col in row: 
+                #    print col            
+            #db.commit()
+        except:
+            db.rollback()
+        db.close
+        return data
+    @staticmethod
+    def getFood(food_id):
+        sqlq = "SELECT id,name FROM foodtest WHERE id==%d" % food_id
+        db = MySQLdb.connect(host=Food.host, port=Food.port, user=Food.user, passwd=Food.pw, db=Food.dbname)
+        cursor = db.cursor()
+        data = None
+        try:
+            cursor.execute(sqlq)
+            data = cursor.fetchall()
+            if len(data)>1:
+                print "error, found %d foods for single id %d" % (len(data),id)
+        except:
+            db.rollback()
+        db.close
+        return data
+    def addFoodToDB(self):
+        sqlq = "INSERT INTO %s (name,energy,carbs,protein,fat,comment) VALUES (\"%s\",%f,%f,%f,%f,\"%s\")" % (Food.tablename,self.name,self.E,self.carbs,self.protein,self.fat,self.comment)
+        db = MySQLdb.connect(host=Food.host, port=Food.port, user=Food.user, passwd=Food.pw, db=Food.dbname)
+        cursor = db.cursor()
+        data = None
+        try:
+            print sqlq
+            cursor.execute(sqlq)
+            db.commit()
+        except:
+            db.rollback()
+        db.close
 
 
 class ActionType:
@@ -55,7 +116,7 @@ class ActionType:
         s += "(press \'q\' to abort)\n" 
         self.Q = s
     def check(self,ans):
-        if int(ans) not in self.D: return self.BADANSWER
+        if ans=="" or int(ans) not in self.D: return self.BADANSWER
         else: return self.GOODANSWER
     def process(self,ans):
         self.d = int(ans)
@@ -70,23 +131,63 @@ class Meal(ActionType):
         self.name = "meal"    
         self.D = {0:"breakfast",1:"lunch",2:"dinner",3:"generic"}
         self.foodList = []
+        self.d = -1
+    def pr(self):
+        print "%s for %s:" % (self.D[self.d],self.person)
+        for f in self.foodList:
+            print f
     def process(self,ans):
+        self.__init__()
         self.d = int(ans)
         print "Add a ", self.D[self.d], " meal by adding a set of food items."
         self.process_food()
+        self.process_name()
+        print "Current meal to be added to DB is:"
+        self.pr()
+        while True:
+            ans = raw_input("Press Y/n to proceed and add meal to DB")
+            if ans=="":
+                print "cannot be empty"
+                continue
+            elif ans=="Y":
+                print "this should add meal to DB!"
+                break
+            else:
+                print "Not adding anyting"
+                break
+        print "done process return "
+        return
+    def process_name(self):
+        ans = raw_input("Enter name (\'q\' to abort) here: ")
+        if ans=="": 
+            print "cannot be empty"
+            self.process_name()
+        self.person = ans
     def process_food(self):
-        print "Add food item.\nData format:"
-        print Food.strFormat
-        ans = raw_input("(\'q\' to abort)\nInput string here: ")
-        if( ans=="q" or ans=="quit"):
+        print "Select food item from list."
+        Food.printFoodList()
+        ans = raw_input("(\'a\' to add new food, \'q\' to abort or finish)\nInput here: ")
+        if ans=="": 
+            print "cannot be empty"
+            self.process_food()
+        elif ans=="q" or ans=="quit":
             return
-        food = Food(ans)
-        if food.ok():
-            print "adding ", food.name
-            self.foodList.append(food)
+        elif ans == "a" or ans == "add":
+            print "Add new food item to DB\nData format:" + Food.strFormat
+            ans = raw_input("(\'q\' to abort)\nInput string here: ")
+            food = Food()
+            food.set(ans)
+            if food.ok():
+                print "adding ", food.name, " to DB"
+                food.addFoodToDB()
         else:
-            print "Something is wrong, not adding this food"
-        process_food()
+            food_id = int(ans)
+            f = Food.getFood(food_id)
+            if f!=None:
+                print "invalid food id ", food_id
+            else:
+                self.foodList.append(food_id)
+        self.process_food()
 
             
 
@@ -130,6 +231,9 @@ if __name__ == "__main__":
     while( True ):
         ans = raw_input(topLevel.Q)
         print "Your answer: ", ans
+        if( ans==""):
+            print "Cannot be empty"
+            continue
         if( ans=="q" or ans=="quit"):
             break;
         if topLevel.check(ans) == topLevel.BADANSWER:
@@ -137,15 +241,15 @@ if __name__ == "__main__":
             continue
         action = topLevel.process(ans)        
         action.build()
-        while( True ):
-            ans = raw_input(action.Q)
-            if( ans=="q" or ans=="quit"):
-                break;
-            if action.check(ans) == action.BADANSWER:
-                print "Choose among the types listed please."
-                continue
-            action.process(ans)
-    
+        ans = raw_input(action.Q)
+        if( ans=="q" or ans=="quit"):
+            continue
+        if action.check(ans) == action.BADANSWER:
+            print "Choose among the types listed please."
+            continue
+        action.process(ans)
+        print "action process done, now Q is ", action.Q
+        
     
             
         
